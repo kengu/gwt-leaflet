a172aeebc4a03b7d7ee0093cef99c539f2f4f17f
Kenneth Gulbrandsøy <kenneth@discofoundation.org>
2012-11-15 10:44:05 +0100

    Removed 'mvn clean' from deploy script

--- build/add_server.py
+++ build/add_server.py
@@ -4,9 +4,9 @@ import os
 import os.path
 import xml.dom.minidom
 
-if os.environ["TRAVIS_SECURE_ENV_VARS"] == "false":
-  print "no secure env vars available, skipping deployment"
-  sys.exit()
+#if os.environ["TRAVIS_SECURE_ENV_VARS"] == "false":
+#  print "no secure env vars available, skipping deployment"
+#  sys.exit()
 
 cwd = os.getcwd()
 print cwd
