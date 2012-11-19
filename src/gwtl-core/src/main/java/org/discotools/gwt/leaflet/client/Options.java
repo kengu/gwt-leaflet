3c2b4c657261d58e903c82e0968fa2483c08956b
Kenneth Gulbrandsøy <kenneth.gulbrandsoy@gmail.com>
2012-11-19 01:10:11 +0100

    Added more Options getters

--- src/gwtl-core/src/main/java/org/discotools/gwt/leaflet/client/Options.java
+++ src/gwtl-core/src/main/java/org/discotools/gwt/leaflet/client/Options.java
@@ -3,6 +3,8 @@ package org.discotools.gwt.leaflet.client;
 import org.discotools.gwt.leaflet.client.jsobject.JSObject;
 import org.discotools.gwt.leaflet.client.jsobject.JSObjectWrapper;
 
+import com.google.gwt.user.client.Element;
+
 /**
  * Options class
  * @author Lionel Leiva-Marcon
@@ -16,6 +18,43 @@ public class Options extends JSObjectWrapper  {
 	public Options() {
 		 this(JSObject.createJSArray());
 	}
+	
+	public JSObject getProperty(String prop) {
+        return getJSObject().getProperty(prop);
+    }
+
+	public String[] getPropertyNames() {
+        return getJSObject().getPropertyNames().split(",");
+    }	
+	
+	public JSObject[] getPropertyAsArray(String prop) {
+        return getJSObject().getPropertyAsArray(prop);
+    }
+	
+	public boolean getPropertyAsBoolean(String prop) {
+        return getJSObject().getPropertyAsBoolean(prop);
+    }
+	
+	public Element getPropertyAsDomElement(String prop) {
+        return getJSObject().getPropertyAsDomElement(prop);
+    }
+	
+	public double getPropertyAsDouble(String prop) {
+        return getJSObject().getPropertyAsDouble(prop);
+    }
+	
+	public float getPropertyAsFloat(String prop) {
+        return getJSObject().getPropertyAsFloat(prop);
+    }
+	
+	public int getPropertyAsInt(String prop) {
+        return getJSObject().getPropertyAsInt(prop);
+    }
+	
+	public String getPropertyAsString(String prop) {
+        return getJSObject().getPropertyAsString(prop);
+    }
+	
 
 	public Options setProperty(String prop, JSObjectWrapper obj) {
         getJSObject().setProperty(prop, obj.getJSObject());
